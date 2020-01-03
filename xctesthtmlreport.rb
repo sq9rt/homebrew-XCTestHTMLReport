6ed3f083738636b2192755764fbe5b16804d7c47
Pavlo Davydiuk <pavlo.davydiuk@miquido.com>
2020-01-03 12:52:11 +0100

    feat(version): bump version to 2.0.0

--- xctesthtmlreport.rb
+++ xctesthtmlreport.rb
@@ -1,8 +1,8 @@
 class Xctesthtmlreport < Formula
   desc "Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/TitouanVanBelle/XCTestHTMLReport"
-  url "https://github.com/TitouanVanBelle/XCTestHTMLReport/releases/download/1.6.1/xchtmlreport-1.6.1.zip"
-  sha256 "8bb7df9bb88953b70560da7e1a67e29e749fecc50917515bb0986a0c620f3015"
+  url "https://github.com/TitouanVanBelle/XCTestHTMLReport/releases/download/2.0.0/xchtmlreport-2.0.0.zip"
+  sha256 "cafd9b4543e85b8c9a4f5c5f59ba1a3b5ecef4e49b73d71f39ec738f074ecdd2"
 
   def install
     bin.install "xchtmlreport"
